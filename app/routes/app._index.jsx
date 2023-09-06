188834c4683c3816919212459bd5da44ae939d40
bartkoz <bartmkoz@gmail.com>
2023-09-06 22:12:24 +0400

    changes

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,5 +1,3 @@
-import { json } from "@remix-run/node";
-import { useActionData, useNavigation, useSubmit } from "@remix-run/react";
 import {
   Page,
   Layout,
@@ -10,57 +8,14 @@ import {
   Text,
 } from "@shopify/polaris";
 
-import { authenticate } from "../shopify.server";
 import Chart from "~/components/chart";
 import TokenBar from "~/components/tokenbar";
 
-export const loader = async ({ request }) => {
-  const { session } = await authenticate.admin(request);
-
-  return json({ shop: session.shop.replace(".myshopify.com", "") });
-};
-
-export async function action({ request }) {
-  const { admin } = await authenticate.admin(request);
-  const response = await admin.graphql(
-    `#graphql
-query {
-  shop {
-    primaryDomain {
-      url
-      host
-    }
-    domains {
-      url
-      host
-    }
-  }
-}`
-  );
-
-  const responseJson = await response.json();
-  console.log(responseJson);
-  return json({
-    product: responseJson.data,
-  });
-}
-
 export default function Index() {
-  const nav = useNavigation();
-  const submit = useSubmit();
-  const actionData = useActionData();
-
-  const isLoading =
-    ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
-
-  const queryProducts = () => submit({}, { replace: true, method: "POST" });
   return (
     <Page>
       <VerticalStack gap="5">
         <Layout>
-          <Layout.Section>
-            <VerticalStack gap="5">{/*<HostSetup />*/}</VerticalStack>
-          </Layout.Section>
           <Layout.Section>
             <Card>
               <HorizontalStack wrap={false}>
@@ -68,14 +23,7 @@ export default function Index() {
                   Tokens used <b>resets in X days</b>
                 </Text>
               </HorizontalStack>
-              {/*<HorizontalStack>*/}
-              {/*  <Text alignment={"start"}>Tokens used</Text>*/}
-              {/*  <Text align={"end"}>Tokens used</Text>*/}
-              {/*</HorizontalStack>*/}
               <TokenBar />
-              <Button loading={isLoading} primary onClick={queryProducts}>
-                query prods
-              </Button>
             </Card>
           </Layout.Section>
           <Layout.Section>
