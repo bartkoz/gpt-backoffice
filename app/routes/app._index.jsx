3d18c8f72310d7fb1840f4d69c50e36baa29784f
bartkoz <bartmkoz@gmail.com>
2023-09-06 22:49:52 +0400

    changes

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -6,12 +6,45 @@ import {
   Button,
   HorizontalStack,
   Text,
+  Box,
 } from "@shopify/polaris";
-
 import Chart from "~/components/chart";
 import TokenBar from "~/components/tokenbar";
+import { json } from "@remix-run/node";
+import { authenticate } from "../shopify.server";
+import { useActionData, useSubmit } from "@remix-run/react";
+export const loader = async ({ request }) => {
+  const { session } = await authenticate.admin(request);
+  return json({ shop: session });
+};
+export async function action({ request }) {
+  const { admin } = await authenticate.admin(request);
+  const response = await admin.graphql(
+    `#graphql
+    query {
+  shop {
+    primaryDomain {
+      url
+      host
+    }
+    domains {
+      url
+      host
+    }
+  }
+}`
+  );
+
+  const responseJson = await response.json();
+  return json({
+    product: responseJson.data,
+  });
+}
 
 export default function Index() {
+  const actionData = useActionData();
+  const submit = useSubmit();
+  const generateProduct = () => submit({}, { replace: true, method: "POST" });
   return (
     <Page>
       <VerticalStack gap="5">
@@ -28,6 +61,35 @@ export default function Index() {
           </Layout.Section>
           <Layout.Section>
             <Chart />
+          </Layout.Section>
+          <Layout.Section>
+            <Card>
+              <VerticalStack gap="5">
+                <HorizontalStack gap="3" align="end">
+                  <Button onClick={generateProduct}>Generate a product</Button>
+                </HorizontalStack>
+                {actionData && (
+                  <Box
+                    padding="4"
+                    background="bg-subdued"
+                    borderColor="border"
+                    borderWidth="1"
+                    borderRadius="2"
+                    overflowX="scroll"
+                  >
+                    <pre style={{ margin: 0 }}>
+                      <code>
+                        {JSON.stringify(
+                          actionData.product.shop.domains,
+                          null,
+                          2
+                        )}
+                      </code>
+                    </pre>
+                  </Box>
+                )}
+              </VerticalStack>
+            </Card>
           </Layout.Section>
         </Layout>
       </VerticalStack>
