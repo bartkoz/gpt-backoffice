642af4bdab75089e81cef4d5072c0822200b78bd
bartkoz <bartmkoz@gmail.com>
2023-09-08 23:38:59 +0400

    changes

--- app/routes/app.preview.jsx
+++ app/routes/app.preview.jsx
@@ -1,5 +1,43 @@
 import { ChatPreview } from "~/components/preview";
+import { authenticate } from "~/shopify.server";
+import { useActionData, useSubmit } from "@remix-run/react";
+import { Page } from "@shopify/polaris";
+import { useEffect } from "react";
+
+export async function action({ request }) {
+  const { admin } = await authenticate.admin(request);
+  const response = await admin.graphql(
+    `#graphql
+    query {
+  shop {
+    primaryDomain {
+      url
+      host
+    }
+    domains {
+      url
+      host
+    }
+  }
+}`
+  );
+
+  const responseJson = await response.json();
+  return responseJson.data.shop;
+}
 
 export default function ProductRecommendations() {
-  return <ChatPreview />;
+  const actionData = useActionData();
+  const submit = useSubmit();
+  const queryGQL = () => {
+    submit({}, { replace: true, method: "POST" });
+  };
+  useEffect(() => {
+    queryGQL();
+  }, []);
+  return (
+    <Page>
+      {actionData && <ChatPreview shop={actionData.primaryDomain.host} />}
+    </Page>
+  );
 }
