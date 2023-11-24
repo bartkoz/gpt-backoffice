973fc683be8d64c9c01f3501767ab36275280b64
bartkoz <bartmkoz@gmail.com>
2023-11-24 18:04:46 +0100

    howto

--- app/routes/app.add-to-theme.jsx
+++ app/routes/app.add-to-theme.jsx
@@ -4,6 +4,10 @@ export default function Setup() {
     <Page>
       <Card>
         <Text>Steps to turn on website</Text>
+        <img
+          src="https://storage.googleapis.com/kip-cdn/howto.png"
+          style={{ maxWidth: "100%", height: "auto" }}
+        />
       </Card>
     </Page>
   );
--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -68,7 +68,7 @@ export default function App() {
             <Link to="/app/settings">Chat settings</Link>
             <Link to="/app/preview">Chat preview</Link>
             <Link to="/app/add-to-theme">Add to theme</Link>
-            <Link to="/app/contact">Support</Link>
+            {/*<Link to="/app/contact">Support</Link>*/}
           </>
         )}
       </ui-nav-menu>
